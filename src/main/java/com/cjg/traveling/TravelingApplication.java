9e72dd2f963f103ca094a4861ea9d14abf5b81a7
cjg <cjg@DESKTOP-UCF321P>
2023-11-03 23:00:22 +0900

    초기화

---
+++ src/main/java/com/cjg/traveling/TravelingApplication.java
@@ -0,0 +1,13 @@
+package com.cjg.traveling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelingApplication.class, args);
+	}
+
+}
