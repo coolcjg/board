b379d406563da97dab5fbb7374524f577d74b8ca
cjg <cjg@DESKTOP-UCF321P>
2023-11-09 21:36:58 +0900

    검증기능 추가

--- src/main/java/com/cjg/traveling/controller/UserController.java
+++ src/main/java/com/cjg/traveling/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.cjg.traveling.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cjg.traveling.domain.User;
 import com.cjg.traveling.dto.UserDTO;
+import com.cjg.traveling.dto.UserDtoInsert;
 import com.cjg.traveling.service.UserService;
 
 @RestController
@@ -16,7 +17,7 @@ public class UserController {
 	UserService userService;
 	
 	@PostMapping("/user")
-	public User insertUser(@RequestBody UserDTO user) {
+	public User insertUser(@Validated(UserDtoInsert.class) UserDTO user) {
 		return userService.insertUser(user);
 	}
 	
--- src/main/java/com/cjg/traveling/dto/UserDTO.java
+++ src/main/java/com/cjg/traveling/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package com.cjg.traveling.dto;
 
 import javax.persistence.Embedded;
+import javax.validation.constraints.NotBlank;
 
 import com.cjg.traveling.domain.Address;
 
@@ -9,10 +10,17 @@ import lombok.Data;
 @Data
 public class UserDTO {
 	
+	@NotBlank(groups = UserDtoUpdate.class)
 	private Long id;
+	
+	@NotBlank(message = "비밀번호를 입력해주세요.", groups = UserDtoInsert.class)
 	private String password;
+	
+	@NotBlank(message = "이름을 입력해주세요.", groups = UserDtoInsert.class)
 	private String name;
+	
 	private String phone;
+	
 	private String commnets;
 	
 	@Embedded
