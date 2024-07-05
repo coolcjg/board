c4baeede58226af99f5209d7b5ed425a8e593e5d
coolcjg <coolcjg@naver.com>
2024-07-05 21:24:45 +0900

    커밋 테스트

--- src/main/java/com/cjg/traveling/TravelingApplication.java
+++ src/main/java/com/cjg/traveling/TravelingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TravelingApplication {
 
+	/*커밋 테스트*/
 	public static void main(String[] args) {
 		SpringApplication.run(TravelingApplication.class, args);
 	}
